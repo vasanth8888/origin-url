b5cb1ff76d4df271f15793d481612297c4f03937
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-09 16:26:24 +0530

    laptops

---
+++ src/test/java/org/org/COMPUTER.java
@@ -0,0 +1,27 @@
+package org.org;
+
+public class COMPUTER {
+	
+	
+	
+	
+	
+	
+	
+	
+	private void mobile1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void mobile2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
